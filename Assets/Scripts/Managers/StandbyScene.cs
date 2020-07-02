875af9ed01102664ee3f3155540c516c893535f4
XxCtrlZxX <harryjung1001@gmail.com>
2020-07-02 13:17:02 +0900

    ButtonsAway, Scene Change to RunningScene

--- Assets/Scripts/Managers/StandbyScene.cs
+++ Assets/Scripts/Managers/StandbyScene.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StandbyScene : MonoBehaviour
 {
@@ -15,12 +16,20 @@ public class StandbyScene : MonoBehaviour
     public Image panel; // fade in
     public GameObject[] buttons;    // 버튼들 배열 저장
 
+    private bool is_buttons_coming;
+
     private void Start()
     {
+        is_buttons_coming = true;
         StartCoroutine(FadeIn());
         StartCoroutine(BirdComing());   // 오리가 걸어옴
         StartCoroutine(CameraMoving()); // 카메라 움직임
         StartCoroutine(ButtonsIn());
+    }
+
+    public void Clicked_PlayBtn()   // 플레이 버튼 누름
+    {
+        StartCoroutine(ButtonsAway("Play"));
     }
 
     IEnumerator BirdComing()
@@ -29,7 +38,7 @@ public class StandbyScene : MonoBehaviour
         player.transform.position = new Vector3(-8, player.transform.position.y, 0);
         while (player.transform.position.x < -3)
         {
-            player.transform.Translate(new Vector3(1.5f, 0, 0) * Time.deltaTime);
+            player.transform.Translate(new Vector3(1f, 0, 0) * Time.deltaTime);
             yield return new WaitForEndOfFrame();
         }
         player.GetComponent<Animator>().SetBool("is_walk", false);   // 애니메이션 멈춤
@@ -40,16 +49,16 @@ public class StandbyScene : MonoBehaviour
     {
         float cx = -2.5f;
         _camera.transform.position = new Vector3(cx, 0, -10);
-        while (_camera.transform.position.x < -0.01)
+        while (_camera.transform.position.x < 0)
         {
             //float r = 1f;
             //cx = Mathf.SmoothDamp(cx, 0, ref r, 100f);
-            cx = Mathf.Lerp(cx, 0, 0.0025f);
+            cx = Mathf.Lerp(cx, 0.1f, 0.0025f);
             _camera.transform.position = new Vector3(cx, 0, -10);
             yield return new WaitForEndOfFrame();
         }
         Debug.Log("카메라 움직임 끝");
-        _camera.transform.position = new Vector3(-0.01f, 0, -10);
+        _camera.transform.position = new Vector3(0f, 0, -10);
     }
 
     IEnumerator FadeIn()
@@ -66,21 +75,42 @@ public class StandbyScene : MonoBehaviour
 
     IEnumerator ButtonsIn()
     {
-		/// 버튼들 화면 "밖에서" 안으로 들어오는 함수
-        while (buttons[0].GetComponent<RectTransform>().anchoredPosition.x > -200)
+        /// 버튼들 화면 "밖에서" 안으로 들어오는 함수
+
+        foreach (GameObject btn in buttons)
+        {
+            btn.GetComponent<RectTransform>().anchoredPosition = new Vector3(350f, btn.GetComponent<RectTransform>().anchoredPosition.y);
+        }
+
+        while (buttons[0].GetComponent<RectTransform>().anchoredPosition.x > -330)
         {
             foreach (GameObject btn in buttons)
             {
-                btn.GetComponent<RectTransform>().anchoredPosition = new Vector3(btn.GetComponent<RectTransform>().anchoredPosition.x - 2, btn.GetComponent<RectTransform>().anchoredPosition.y);
+                btn.GetComponent<RectTransform>().anchoredPosition = new Vector3(btn.GetComponent<RectTransform>().anchoredPosition.x - 200 * Time.deltaTime, btn.GetComponent<RectTransform>().anchoredPosition.y);
             }
+
+            if (is_buttons_coming == false)
+                break;
+
 			yield return new WaitForEndOfFrame();
         }
     }
 
-    IEnumerator ButtonsAway()
+    IEnumerator ButtonsAway(String flag)
     {
+        is_buttons_coming = false;
         /// 버튼들 화면 밖으로 나가는 함수
-        yield return null;
+        while (buttons[0].GetComponent<RectTransform>().anchoredPosition.x < 350)
+        {
+            foreach (GameObject btn in buttons)
+            {
+                btn.GetComponent<RectTransform>().anchoredPosition = new Vector3(btn.GetComponent<RectTransform>().anchoredPosition.x + 450 * Time.deltaTime, btn.GetComponent<RectTransform>().anchoredPosition.y);
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (flag == "Play") // flag가 Play면 RunningScene으로 넘어감
+            SceneManager.LoadScene("RunningScene");
     }
 
 }
