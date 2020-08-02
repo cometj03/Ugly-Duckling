a77262e1e7eca6f5073063679a040ebc808f89e5
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-02 19:24:35 +0900

    [Edit] MainScene의 FadeIn -> animation

---
+++ Assets/Scripts/Managers/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator Transition;
+    
+    private static readonly int Start = Animator.StringToHash("Start");
+    private float transitionTime = 1f;
+
+    public void LoadNextLevel(int levelIndex)
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    private IEnumerator LoadLevel(int levelIndex)
+    {
+        Transition.SetTrigger(Start);
+        
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
--- Assets/Scripts/Managers/MainScene.cs
+++ Assets/Scripts/Managers/MainScene.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -12,41 +11,47 @@ public class MainScene : MonoBehaviour
 
 	public Text text;	// touch to start Text
 	public GameObject player;	// 오리 객체 
-	public Image panel;	// 화면을 덮어주는 패널
+	public GameObject canvas;	// FadeIn하는 이미지
 
 	private bool fade = true;   // touch to start 가 흐려지게 하건지 뚜렷해지게 할건지 여부
+	private static readonly int IsWalk = Animator.StringToHash("is_walk");	// 애니메이션 is_walk 트리거 저장
+	private IEnumerator _touchToStart;
 
 	private void Awake()
     {
-		Screen.SetResolution(Screen.height * 16 / 9, Screen.height, true);
+		// Screen.SetResolution(Screen.height * 16 / 9, Screen.height, true);
 		// Debug.Log(Screen.width + ", " + Screen.height);
     }
 
     private void Start()
     {
-		panel.color = new Color(0, 0, 0, 0);
+	    _touchToStart = TouchToStart();
     }
 
     private void Update()
 	{
-		/// touch to start가 깜박거리는 효과를 주는 코드
+		// touch to start가 깜박거리는 효과를 주는 코드
 		if (fade)
 		{
-			text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - Time.deltaTime * 0.8f);
+			var color = text.color;
+			color = new Color(color.r, color.g, color.b, color.a - Time.deltaTime * 0.8f);
+			text.color = color;
 			if (text.color.a <= 0)
 				fade = false;
 		}
 		else
 		{
-			text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + Time.deltaTime * 0.8f);
+			var color = text.color;
+			color = new Color(color.r, color.g, color.b, color.a + Time.deltaTime * 0.8f);
+			text.color = color;
 			if (text.color.a >= 1)
 				fade = true;
 		}
 
-		/// 프레이어가 화면 터치시 오리가 화면 밖으로 나가고 다음 씬으로 넘어감
+		// 프레이어가 화면 터치시 오리가 화면 밖으로 나가고 다음 씬으로 넘어감
 		if (Input.touchCount > 0 || Input.anyKeyDown) 
 		{
-			StartCoroutine(TouchToStart());
+			StartCoroutine(_touchToStart);
 		}
 	}
 
@@ -54,12 +59,12 @@ public class MainScene : MonoBehaviour
 	{
 		float birdSpeed = 1.5f;
 		player.GetComponent<SpriteRenderer>().flipX = true;
-		player.GetComponent<Animator>().SetBool("is_walk", true);
+		player.GetComponent<Animator>().SetBool(IsWalk, true);
 		while (player.transform.position.x < 6)
 		{
 			player.transform.Translate(new Vector3(birdSpeed, 0, 0) * Time.deltaTime);
 			
-			/// 가는 도중 한 번 더 화면 터치하면 속도 증가
+			// 가는 도중 한 번 더 화면 터치하면 속도 증가
 			if (Input.touchCount > 0)
 			{
 				Touch touch = Input.GetTouch(0);
@@ -68,19 +73,10 @@ public class MainScene : MonoBehaviour
 			} else if (Input.anyKeyDown)
 				birdSpeed = 2.5f;
 
-			yield return new WaitForEndOfFrame();
+			yield return null;
+			// yield return new WaitForEndOfFrame();
 		}
-		/// FadeIn 후, 다음 씬으로 넘어감
-		StartCoroutine(FadeOut());
+		// 다음 씬으로 넘어감
+		canvas.GetComponent<LevelLoader>().LoadNextLevel(SceneManager.GetActiveScene().buildIndex + 1);
 	}
-
-	IEnumerator FadeOut()
-    {
-		while (panel.color.a < 0.95f)
-		{
-			panel.color = new Color(0, 0, 0, panel.color.a + Time.deltaTime);
-			yield return new WaitForEndOfFrame();
-		}
-		SceneManager.LoadScene("StandbyScene");
-    }
 }
