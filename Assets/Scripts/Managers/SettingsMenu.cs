a45e9a0ea6bfb3e258fe69482cafebc249e6e35c
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-05 16:11:24 +0900

    [Add] volume slider

---
+++ Assets/Scripts/Managers/SettingsMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Text masterVolumeText;
+    public Text musicVolumeText;
+    public Text effectVolumeText;
+
+    public void SetMasterVolume(float volume)
+    {
+        masterVolumeText.text = (volume * 100).ToString("F0") + "%";
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolumeText.text = (volume * 100).ToString("F0") + "%";
+    }
+    
+    public void SetEffectVolume(float volume)
+    {
+        effectVolumeText.text = (volume * 100).ToString("F0") + "%";
+    }
+}
