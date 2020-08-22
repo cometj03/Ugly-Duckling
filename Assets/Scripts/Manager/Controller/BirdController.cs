bb420e5277a77c825c037cf00cd33e3c7e2296d5
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-22 20:38:35 +0900

    2020.08.22

--- Assets/Scripts/Manager/Controller/BirdController.cs
+++ Assets/Scripts/Manager/Controller/BirdController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdControler : MonoBehaviour
+public class BirdController : MonoBehaviour
 {
 	public GameObject manager;
 	
