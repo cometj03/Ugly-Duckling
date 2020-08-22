028fc5cc9a1a0d27626e45278fd00ff2fc73ff30
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-22 20:34:35 +0900

    block drag and drop

---
+++ Assets/Scripts/Object/Block/DragAndDrop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Camera _camera;
+
+    private Vector3 posOffset;
+    private bool isDragging;
+
+    private void Start()
+    {
+        _camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void OnMouseDown()
+    {
+        posOffset = _camera.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position;
+        posOffset.z = 0;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 point = _camera.ScreenToWorldPoint(Input.mousePosition);
+        point.z = gameObject.transform.position.z;
+        gameObject.transform.position = point - posOffset;
+    }
+}
