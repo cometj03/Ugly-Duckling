1d4993fac826eb0692384831bbf084be1740f941
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-09-06 22:21:22 +0900

    Puzzle resolve movement error patch

--- Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -13,9 +13,9 @@ public class PuzzleManager : MonoBehaviour
 	Puzzle selectPuzzle;
 	Block selectBlock;
 
-    List<Puzzle> puzzles = new List<Puzzle>();
+	List<Puzzle> puzzles = new List<Puzzle>();
 
-    public void Push(Puzzle puzzle)
+	public void Push(Puzzle puzzle)
 	{
 		puzzles.Add(puzzle);
 	}
@@ -34,49 +34,55 @@ public class PuzzleManager : MonoBehaviour
 				mouseBlockDIstance = selectBlock.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			}
 		}
-		else if (Input.GetMouseButton(0))
+		if (selectPuzzle)
 		{
-			selectBlock.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseBlockDIstance;
-		}
-		else if (Input.GetMouseButtonUp(0))
-		{
-			selectBlock.transform.position = (Vector2)math.round((Vector2)selectBlock.transform.position);
-
-			foreach(var tile in selectBlock.tiles)
+			if (Input.GetMouseButton(0))
 			{
-				int scrossPoint = 0;
+				selectBlock.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseBlockDIstance;
+			}
+			else if (Input.GetMouseButtonUp(0))
+			{
+				selectBlock.transform.position = (Vector2)math.round((Vector2)selectBlock.transform.position);
 
-				foreach(var outlinetile in selectPuzzle.outline.tiles)
+				foreach (var tile in selectBlock.tiles)
 				{
-					if(tile.transform.position.y == outlinetile.transform.position.y && tile.transform.position.x <= outlinetile.transform.position.x)
+					int scrossPoint = 0;
+
+					foreach (var outlinetile in selectPuzzle.outline.tiles)
 					{
-						scrossPoint++;
+						if (tile.transform.position.y == outlinetile.transform.position.y && tile.transform.position.x <= outlinetile.transform.position.x)
+						{
+							scrossPoint++;
+						}
+					}
+
+					if (scrossPoint % 2 == 0)
+					{
+						selectBlock.transform.position = perBlockposition;
+						return;
 					}
 				}
 
-				if(scrossPoint % 2 == 0)
+				foreach (var block in selectPuzzle.blocks)
 				{
-					selectBlock.transform.position = perBlockposition;
-					return;
-				}
-			}
-
-			foreach(var block in selectPuzzle.blocks)
-			{
-				if(block != selectBlock)
-				{
-					foreach(var tile in selectBlock.tiles)
+					if (block != selectBlock)
 					{
-						foreach(var anotertile in block.tiles)
+						foreach (var tile in selectBlock.tiles)
 						{
-							if(tile.transform.position == anotertile.transform.position)
+							foreach (var anotertile in block.tiles)
 							{
-								selectBlock.transform.position = perBlockposition;
-								return;
+								if (tile.transform.position == anotertile.transform.position)
+								{
+									selectBlock.transform.position = perBlockposition;
+									return;
+								}
 							}
 						}
 					}
 				}
+
+				selectPuzzle = null;
+				selectBlock = null;
 			}
 		}
 	}
