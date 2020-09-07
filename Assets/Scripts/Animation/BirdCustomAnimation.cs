24a66a9c462b63cf06c3d63c68179b867e9d1e62
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-09-07 20:31:40 +0900

    BridCustomAnimation Compelete

---
+++ Assets/Scripts/Animation/BirdCustomAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class BirdCustomAnimation : MonoBehaviour
+{
+    public string skinName;
+
+	int animationPhase = 0;
+	public bool isWalking = false;
+
+    Sprite[] idle;
+	Sprite[] walk;
+
+	SpriteRenderer player;
+
+	private void Awake()
+	{
+		idle = Resources.LoadAll<Sprite>("Animations/" + skinName + "/Idle");
+		walk = Resources.LoadAll<Sprite>("Animations/" + skinName + "/Walk");
+		player = GetComponent<SpriteRenderer>();
+
+		StartCoroutine(DoAnimation());
+	}
+
+	IEnumerator DoAnimation()
+	{
+		while (true)
+		{
+			if(animationPhase > (isWalking ? walk : idle).Length - 1)
+			{
+				animationPhase = 0;
+			}
+
+			if (isWalking)
+			{
+				player.sprite = walk[animationPhase];
+			}
+			else
+			{
+				player.sprite = idle[animationPhase];
+			}
+
+			animationPhase++;
+			yield return new WaitForSeconds(isWalking ? 0.1f : 0.3f);
+		}
+	}
+}
