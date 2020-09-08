e6783c1f170861c5056f764552e900a6294cd1bd
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-09-08 22:25:10 +0900

    Now, Cant move blocks while moving

--- Assets/Scripts/Object/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Object/Puzzle/PuzzleManager.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PuzzleManager : MonoBehaviour
 {
 	public GameObject block;
 	public GameObject previewTile;
+
+	public GameObject jumpObject;
+	public GameObject moveObject;
+
+	Image jumpImage;
+	Image moveImage;
+
+	Button jumpButton;
+	HorizontalButton moveButton;
 
 	Vector2 perBlockposition;
 	Vector2 mouseBlockDIstance;
@@ -36,6 +46,12 @@ public class PuzzleManager : MonoBehaviour
 				perBlockposition = selectBlock.transform.localPosition;
 				mouseBlockDIstance = selectBlock.transform.localPosition - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+				jumpImage.color = new Color(0.5f, 0.5f, 0.5f);
+				moveImage.color = new Color(0.5f, 0.5f, 0.5f);
+
+				jumpButton.enabled = false;
+				moveButton.enabled = false;
+
 				previewBlock = Instantiate(block).GetComponent<Block>();
 
 				foreach (var tile in selectBlock.tiles)
@@ -57,6 +73,12 @@ public class PuzzleManager : MonoBehaviour
 			}
 			else if (Input.GetMouseButtonUp(0))
 			{
+				jumpImage.color = new Color(1, 1, 1);
+				moveImage.color = new Color(1, 1, 1);
+
+				jumpButton.enabled = true;
+				moveButton.enabled = true;
+
 				selectBlock.transform.localPosition = (Vector2)math.round((Vector2)previewBlock.transform.localPosition);
 				Destroy(previewBlock.gameObject);
 
@@ -108,6 +130,14 @@ public class PuzzleManager : MonoBehaviour
 				selectBlock = null;
 			}
 		}
+	}
+
+	private void Awake()
+	{
+		jumpImage = jumpObject.GetComponent<Image>();
+		jumpButton = jumpObject.GetComponent<Button>();
+		moveImage = moveObject.GetComponent<Image>();
+		moveButton = moveObject.GetComponent<HorizontalButton>();
 	}
 
 	private void Update()
