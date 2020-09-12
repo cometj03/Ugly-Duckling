41b28beb4ed631892f46135f5ec05556bfdf0130
XxCtrlZxX <harryjung1001@gmail.com>
2020-09-12 11:22:14 +0900

    sound

---
+++ Assets/Scripts/Manager/UI/VolumeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour
+{
+    public AudioMixer _audioMixer;
+    
+    public void SetMusicVolume(float volume)
+    {
+        _audioMixer.SetFloat("MusicVolume", volume);
+    }
+    
+    public void SetEffectVolume(float volume)
+    {
+        //print((volume * 100).ToString("F0") + "%");
+    }
+}
