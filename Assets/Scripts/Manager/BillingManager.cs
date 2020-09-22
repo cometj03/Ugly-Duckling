28067bd0993ebc29418bdbc53b2db57c338a13b1
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-09-23 01:52:00 +0900

    Adding In game purchase

---
+++ Assets/Scripts/Manager/BillingManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using UnityEditor.Purchasing;
+using UnityEngine;
+
+public class BillingManager : MonoBehaviour
+{
+	private void Awake()
+	{
+
+	}
+}
