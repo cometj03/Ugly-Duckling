06aeee1eb62ede3c1ac29392e7d363022d644f0a
XxCtrlZxX <harryjung1001@gmail.com>
2020-09-23 17:27:36 +0900

    Separated Stages

--- Assets/Scripts/Manager/UI/UIGamePanel.cs
+++ Assets/Scripts/Manager/UI/UIGamePanel.cs
@@ -7,7 +7,7 @@ public class UIGamePanel : MonoBehaviour
 {
     public GameObject PausePanel, ClearPanel, OverPanel;
 
-    private bool isPause, isClear, isOver;
+    private bool isPause;
 
     private CanvasGroup gamePanelCanvasGroup;
     
@@ -60,8 +60,30 @@ public class UIGamePanel : MonoBehaviour
     }
 
     public void ExitBtn() => FindObjectOfType<LevelLoader>().LoadNextLevel("StandbyScene");
-    public void NextStageBtn() => FindObjectOfType<LevelLoader>().LoadNextLevel(SceneManager.GetActiveScene().buildIndex + 1);
 
+    public void NextStageBtn()
+    {
+        int level = PlayerData.Instance.currentLevel;
+        
+        PlayerData.Instance.currentLevel++; // 레벨 1 더함
+        
+        switch (level)
+        {
+            case 10:
+                FindObjectOfType<LevelLoader>().LoadNextLevel("2_Autumn");
+                break;
+            case 20:
+                FindObjectOfType<LevelLoader>().LoadNextLevel("3_Winter");
+                break;
+            case 30:
+                ExitBtn();  // 준비화면으로 나감
+                break;
+            default:
+                FindObjectOfType<LevelLoader>().LoadNextLevel(SceneManager.GetActiveScene().buildIndex);
+                break;
+        }
+    }
+    
     IEnumerator PanelFadeIn()
     {
         float alpha = 0;
--- Assets/Scripts/Object/Obstacle/WolfSign.cs
+++ Assets/Scripts/Object/Obstacle/WolfSign.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class WolfSign : MonoBehaviour
@@ -7,9 +8,10 @@ public class WolfSign : MonoBehaviour
     public Sprite[] waringAnimation;
 
     private Transform birdTarget;
-    private bool isInstance;
-
     SpriteRenderer waringMark;
+
+    bool isInstance, isAnim;
+
     
     void Start()
     {
@@ -19,23 +21,45 @@ public class WolfSign : MonoBehaviour
 
     void Update()
     {
-        if (birdTarget.position.x - transform.position.x > 4 && !isInstance)
+        if (birdTarget.position.x > transform.position.x && !isAnim)   // 오리가 표지판을 지날 때
+        {
+            isAnim = true;
+            StartCoroutine(DoSignAnim());
+        } 
+        else if (birdTarget.position.x < transform.position.x && isAnim)    // 오리가 다시 표지판 밖으로 나갈 때
+        {
+            Debug.Log("밖으로 나감");
+            isAnim = false;
+            StartCoroutine(DoSignCloseAnim());
+        }
+
+        if (birdTarget.position.x - transform.position.x > 4 && !isInstance)    // 늑대 생성될 때
         {
             Vector3 wolfPos = transform.position;
             wolfPos.x -= 6;
             Instantiate(wolfPrefab, wolfPos, Quaternion.identity);
             isInstance = true;
 
-            StartCoroutine(DoSignAnim());
+            waringMark.sprite = waringAnimation[waringAnimation.Length - 1];
         }
     }
 
     IEnumerator DoSignAnim()
     {
-        for (int i = 0; i < waringAnimation.Length; i++)
+        for (int i = 0; i < waringAnimation.Length - 1; i++)
         {
             waringMark.sprite = waringAnimation[i];
             yield return new WaitForSeconds(0.2f);
         }
+    }
+
+    IEnumerator DoSignCloseAnim()
+    {
+        for (int i = waringAnimation.Length - 2; i >= 0; i--)
+        {
+            waringMark.sprite = waringAnimation[i];
+            yield return new WaitForSeconds(0.2f);
+        }
+        waringMark.sprite = null;
     }
 }
